05e5a0aace99d845d1d6aac0ad5b3b93b759e2fd
Veritas <ochigbogodswill868@gmail.com>
2025-11-07 13:14:11 -0800

    added protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Messages from './Admin/Messages'
 import Products from './Admin/Products'
 import Settings from './Admin/Settings'
 import { AppProvider } from './context/AppContext'
+import AdminRoute from './Auth/AdminRoute'
 
 function App() {
 
@@ -23,10 +24,10 @@ function App() {
         </Route>
           {/* admin routes */}
         <Route element={<AdminLayout/>}>
-          <Route path='/admin' element={<Dashboard/>}/>
-          <Route path='/admin/messages' element={<Messages/>}/>
-          <Route path='/admin/products' element={<Products/>}/>
-          <Route path='/admin/settings' element={<Settings/>}/>
+          <Route path='/admin' element={<AdminRoute><Dashboard/></AdminRoute>}/>
+          <Route path='/admin/messages' element={<AdminRoute><Messages/></AdminRoute>}/>
+          <Route path='/admin/products' element={ <AdminRoute><Products/></AdminRoute>}/>
+          <Route path='/admin/settings' element={ <AdminRoute><Settings/></AdminRoute>}/>
         </Route>
           <Route path='/login/7cOD7LQu9ajR4VkP9zU1qtiW26EtAlwhNwWm0ogGt45t8f3IJPFnoIkU2fbA' element={<LoginForm/>}/>
       </Routes>
---
+++ src/Auth/AdminRoute.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+
+const AdminRoute = ({ children }) => {
+  const token = localStorage.getItem("token")
+  const user = JSON.parse(localStorage.getItem("user"))
+
+  if (!token || !user || user.role !== "admin") {
+    return <Navigate to="/login/7cOD7LQu9ajR4VkP9zU1qtiW26EtAlwhNwWm0ogGt45t8f3IJPFnoIkU2fbA" />
+  }
+
+  return children
+}
+
+export default AdminRoute
