56afac2c7c5a79a21851997e5f5c163eae27614e
Veritas <ochigbogodswill868@gmail.com>
2025-11-07 13:17:52 -0800

    updated route logic

--- src/Auth/AdminRoute.jsx
+++ src/Auth/AdminRoute.jsx
@@ -5,7 +5,7 @@ const AdminRoute = ({ children }) => {
   const token = localStorage.getItem("token")
   const user = JSON.parse(localStorage.getItem("user"))
 
-  if (!token || !user || user.role !== "admin") {
+  if (!token || !user ) {
     return <Navigate to="/login/7cOD7LQu9ajR4VkP9zU1qtiW26EtAlwhNwWm0ogGt45t8f3IJPFnoIkU2fbA" />
   }
 
